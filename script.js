4e358ba9062b39fc31a264e4d34bbead6b04606d
Erlend Svanevik <svme1124@gmail.com>
2024-11-20 11:22:01 +0100

    Inital commit

---
+++ script.js
@@ -0,0 +1,48 @@
+let userName = "John";
+let userAge = 20;
+let userCity = "Bergen";
+
+if (userAge > 18) {
+  console.log("Du er over 18, velkommen inn!");
+} else if (userAge <= 18 || userCity === "Oslo") {
+  console.log("Du er ikke gammel nok, og du er fra feil by");
+}
+if (userName === "John" && userCity === "Bergen") {
+  console.log(`Velkommen inn, ${userName} fra ${userCity}!`);
+}
+
+/**
+ * The calculator takes in 3 parameters
+ * @typedef {operators}
+ * @param {number} num1 - Number
+ * @param {number} num2 - Number
+ * @param {"+" | "-" | "*" | "/"} operator - String with `+` | `-` | `*` | `/`
+ * @example calculator(30, 60, "+")
+ * @returns {number|string} `90`, but in some cases if the user fails to provide a valid operator, or no operator at all, it will result in a error message.
+ * @example "Please provide a operator..." or "Please provide a valid operator..."
+ */
+const calculator = (num1, num2, operator) => {
+  console.log(num1, num2, operator);
+  switch (operator) {
+    case "+":
+      return num1 + num2;
+
+    case "-":
+      return num1 - num2;
+
+    case "*":
+      return num1 * num2;
+
+    case "/":
+      return num1 / num2;
+
+    case undefined:
+      return "Please provide a operator...";
+    default:
+      return "Please provide a valid operator...";
+  }
+};
+
+const calculatorResult = calculator(7, 7, "*");
+
+console.log(calculatorResult);
